cdc7be7147d9d377512fbb5e73132c3d51d1ef6a
ettiev <126219891+ettiev@users.noreply.github.com>
2023-12-01 20:17:32 +0200

    Added Type Casting
    added type casting on req.body and req.params

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 
 import { Todo } from "../models/todo";
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string};
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -11,9 +14,10 @@ router.get("/", (req, res, next) => {
 })
 
 router.post("/todo", (req, res, next) => {
+    const body = req.body as RequestBody;
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text 
+        text: body.text 
     };
     todos.push(newTodo);
     res.status(201).json({
@@ -24,12 +28,14 @@ router.post("/todo", (req, res, next) => {
 })
 
 router.put("/todo/:todoId", (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text
+            text: body.text
         }
         return res.status(200).json({
             message: "Updated the todo",
@@ -40,7 +46,8 @@ router.put("/todo/:todoId", (req, res, next) => {
 })
 
 router.delete("/todo/:todoId", (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
+    const params = req.params as RequestParams;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId)
     res.status(200).json({
         message: "Deleted the todo",
         todos: todos
