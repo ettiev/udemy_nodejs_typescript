0a8f7b229f2b331e956f6ba7109a5b5327d1a938
ettiev <126219891+ettiev@users.noreply.github.com>
2023-12-01 18:01:21 +0200

    use Union Types
    Function can add numbers or strings and console log the results

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var num1Element = document.getElementById("num1");
-var num2Element = document.getElementById("num2");
-var buttonElement = document.querySelector("button");
+"use strict";
+const num1Element = document.getElementById("num1");
+const num2Element = document.getElementById("num2");
+const buttonElement = document.querySelector("button");
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2;
+    }
+    else {
+        return +num1 + +num2;
+    }
 }
-// add(1, 6);
-// add("1", "6"); - Does not work because of strict type checking
-buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener("click", function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
+buttonElement.addEventListener("click", () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+    const stringResult = add(num1, num2);
     console.log(result);
+    console.log(stringResult);
 });
--- app.ts
+++ app.ts
@@ -1,16 +1,23 @@
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
-const buttonElement = document.querySelector("button")
+const buttonElement = document.querySelector("button")!;
 
-function add(num1: number, num2: number): number {
-    return num1 + num2
+function add(num1: number | string, num2: number | string) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    } else if (typeof num1 === "string" && typeof num2 === "string") {  
+        return num1 + " " + num2
+    } else {
+        return +num1 + +num2;
+    }
 }
-// add(1, 6);
-// add("1", "6"); - Does not work because of strict type checking
+    
 
-buttonElement?.addEventListener("click", () =>{
+buttonElement.addEventListener("click", () =>{
     const num1 = num1Element.value;
     const num2 = num2Element.value;
-    const result = add(+num1, +num2); 
+    const result = add(+num1, +num2);
+    const stringResult = add(num1, num2); 
     console.log(result);
+    console.log(stringResult)
 });
